68b8e5c233a2876d905bc0d30c8975137f5fbc4f
konsole-log <anushshrestha00@gmail.com>
2026-08-19 23:34:39 +0545

    Writing some basic for the ouput of the Interpreter

--- Flux.CLI/Program.cs
+++ Flux.CLI/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Flux.CLI;
+
+public class Flux
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: flux [script]");
+            Environment.Exit(64);
+        }
+        else if (args.Length == 1) { 
+            RunFile(args[0]);
+        }else{
+            RunPrompt();
+        }
+    }
+    private static void RunFile(string path){
+        if(!File.Exists(path)){
+            Console.Error.WriteLine($"File not found : {path}");
+            return;
+        }
+        Run(File.ReadAllText(path));
+    }
+    private static void RunPrompt(){
+
+    }
+    private static void Run(string path){
+
+    }
+}
---
+++ Flux.Language/Lexer/Lexer.cs
@@ -0,0 +1,10 @@
+namespace Flux.Language.Lexer
+{
+    public class Lexer
+    {
+        public void Test()
+        {
+            Console.WriteLine("Flux lexer");
+        }
+    }
+}
